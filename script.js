03d00ec3556c4276362482902073aea4cd32c279
Shaira <umayamgmail.com>
2025-07-02 15:00:41 +0800

    Fix index file script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validateForm() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  if (!email || !password) {
+  if (email  === ""|| !password === "") {
     alert("Both fields are required!");
     return false;
   }
