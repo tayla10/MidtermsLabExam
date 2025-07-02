e51a91d25fe0f4aa80f7673bf5b2a6d58202bc79
Shaira <umayamgmail.com>
2025-07-02 14:44:22 +0800

    Add JS form validation

---
+++ script.js
@@ -0,0 +1,9 @@
+function validateForm() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  if (!email || !password) {
+    alert("Both fields are required!");
+    return false;
+  }
+  return true;
+}
