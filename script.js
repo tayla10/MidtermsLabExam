880c65cf31ceea9867a10540aa81890795b051cb
Shaira <umayamgmail.com>
2025-07-02 15:03:09 +0800

    Fix bugs in js.file

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function validateForm() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  if (email  === ""|| !password === "") {
+
+  if (email === "" || password === "") {
     alert("Both fields are required!");
     return false;
   }
+
   return true;
 }
